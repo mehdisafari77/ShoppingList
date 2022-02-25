8c9c7b70de7db4899bceb2e8437dbc1ff4f8729a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-25 13:29:31 -0800

    ItemDetaiVC added

--- ShoppingList/Controllers/ItemDetailViewController.swift
+++ ShoppingList/Controllers/ItemDetailViewController.swift
@@ -9,21 +9,31 @@ import UIKit
 
 class ItemDetailViewController: UIViewController {
 
+    @IBOutlet weak var itemNameLabel: UITextField!
+    @IBOutlet weak var itemNotesLabel: UITextView!
+    
+    var item: Item?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButton(_ sender: Any) {
+        guard let name = itemNameLabel.text, !name.isEmpty else { return }
+        
+        if let item = item {
+            ItemController.shared.updateShoppingItem(item: item, name: name, notes: itemNotesLabel.text)
+        } else {
+            ItemController.shared.createShoppingItem(name: name, notes: itemNotesLabel.text)
+        }
+        navigationController?.popViewController(animated: true)
     }
-    */
+
+    func updateViews() {
+        guard let item = item else { return }
+        itemNameLabel.text = item.name
+        itemNotesLabel.text = item.notes
+    }
 
 }
